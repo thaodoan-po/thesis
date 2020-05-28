ea3867e8502fbaff36bd6524f744e59772ba329b
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-05-29 02:06:54 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Topbar from './component/Topbar';
 import Footer from './component/Footer';
@@ -7,46 +7,112 @@ import Card from './component/Card';
 import Linechart from './component/Linechart';
 import Piechart from './component/Piechart';
 import Process from './component/Process';
+import db from './firebase';
+import Port from './component/Port';
 
-function App() {
-  return (
-    <div className="App">
-      
-        <Topbar/>
-        <div className="container-fluid">
-          <Pageheading title="Dashboard"/>
-          
-          {/* Content row */}
-          <div className="row">
-
-            {/* Card listing */}
-            <Card title="Avarage CPU" color="primary" num="8%" icon="microchip" />
-            <Card title="Total Network" color="success" num="0,32s" icon="rss" />
-            <Card title="Avarage Ram" color="info" num="40%" icon="server" />
-            <Card title="Avarage Response Time" color="warning" num="119mbps" icon="clock" />
-
-          </div>
-
-          {/* Chart area */}
-          <div className="row">
-            <div className="col-xl-8 col-lg-7">
-              <Linechart title="Networking"></Linechart>
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loadAvg: '',
+      memUsed: '',
+      total:'',
+      cpu: ''
+    };
+  }
+  getLoadAvg = () => {
+    let ref = db.ref('monitor/users/001/domain/001/loadAvg/onesec');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      console.log(state);
+      this.setState ({
+        loadAvg: state + "s"
+      });
+    });
+    console.log('DATA RETRIEVED');
+  }
+  getUsedMem = () => {
+    let ref = db.ref('monitor/users/001/domain/001/mem/used');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      console.log(state);
+      this.setState ({
+        memUsed: state + "%"
+      });
+    });
+    console.log('DATA RETRIEVED');
+  }
+  getTotalProc = () => {
+    let ref = db.ref('/monitor/users/001/domain/001/totalProc/total');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      console.log(state);
+      this.setState ({
+        total: state
+      });
+    });
+    console.log('DATA RETRIEVED');
+  }
+  getCpu = () => {
+    let ref = db.ref('monitor/users/001/domain/001/cpu/cpus');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      console.log(state);
+      this.setState ({
+        cpu: state + "%"
+      });
+    });
+    console.log('DATA RETRIEVED');
+  }
+  componentDidMount() {
+    this.getLoadAvg();
+    this.getUsedMem();
+    this.getTotalProc();
+    this.getCpu();
+  }
+  render(){
+    return (
+      <div className="App">
+        
+          <Topbar/>
+          <div className="container-fluid">
+            <Pageheading title="Dashboard"/>
+            
+            {/* Content row */}
+            <div className="row">
+  
+              {/* Card listing */}
+              <Card title="CPU Utilization" color="primary" num={this.state.cpu} icon="microchip" />
+              <Card title="Load Averages" color="success" num={this.state.loadAvg} icon="clock" />
+              <Card title="Used Memory" color="info" num={this.state.memUsed} icon="server" />
+              <Card title="Running Process" color="warning" num={this.state.total} icon="list" />
+  
             </div>
-            <div className="col-xl-4 col-lg-5">
-              <Piechart title="CPU"></Piechart>
+  
+            {/* Chart area */}
+            <div className="row">
+              <div className="col-xl-8 col-lg-7">
+                <Linechart title="Networking"></Linechart>
+              </div>
+              <div className="col-xl-4 col-lg-5">
+                <Piechart title="Disk"></Piechart>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div class="col-lg-6 mb-4">
-              <Process></Process>
+            <div className="row">
+              <div class="col-lg-6 mb-4">
+                <Process></Process>
+              </div>
+              <div class="col-lg-6 mb-4">
+                <Port></Port>
+              </div>
             </div>
+  
           </div>
-
-        </div>
-        <Footer/>
-
-    </div>
-  );
+          <Footer/>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyC5-UG6egEihQyr-PoDfvUUNP8Wfvvvp3I",
+    authDomain: "thaodoan-thesis.firebaseapp.com",
+    databaseURL: "https://thaodoan-thesis.firebaseio.com",
+    projectId: "thaodoan-thesis",
+    storageBucket: "thaodoan-thesis.appspot.com",
+    messagingSenderId: "565251832636",
+    appId: "1:565251832636:web:e468f4c7b7eb6f613a7c39",
+    measurementId: "G-CH6ZHX4JP4"
+}
+const app = firebase.initializeApp(config);
+const db = app.database();
+//let ref = db.ref('monitor/users/001/domain/001/disk');
+// var array = [];
+// ref.on('value', snapshot => {
+//     var lable = snapshot.child('lable').val();
+//     var toal = snapshot.child('total').val();
+//     var use = snapshot.child('use').val();
+//     var remain = snapshot.child('remain').val();
+//     var numUse = parseInt(use, 10)/parseInt(toal, 10)*100;
+//     var numRemain = 100 - numUse;
+//     array.push(`{name: used, value:`+ numUse +`}`, `{name: remain, value: ` + numRemain + `}`);
+//     console.log(array);
+// // });
+//     let port = db.ref('monitor/users/001/domain/001/port');
+//     port.on('value', snapshot => {
+//         console.log(snapshot.val());
+//         console.log('DATA RETRIEVED');
+//     });
+        let ref = db.ref('monitor/users/001/domain/001/topProcess').orderByChild('mem');
+        ref.on('value', snapshot => {
+            var state = snapshot.val();
+            console.log(state);
+        });
+export default db;
+    
