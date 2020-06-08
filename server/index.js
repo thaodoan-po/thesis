2daa6b14eb37763c1cbc5da2ecc6826d8fe7cc79
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-06-08 11:01:10 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,75 +1,36 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const SSH = require('simple-ssh');
-var schedule = require('node-schedule');
-var data = require('./getData');
-//var network = require('./getNetwork');
-
-// const PORT = process.env.PORT || 4001; 
-
-// const router = require('./router');
-
-// const app = express();
-// const sever = http.createServer(app);
-// const io = socketio(sever);
-
-// io.on('connection', (socket) => {
-//     console.log('We have a new connection');
-//     socket.on('disconnect', () => {
-//         console.log('User had left');
-//     });
-// });
-
-// app.use(router);
-
-// app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
-
-var ssh = new SSH({
-  host: '192.168.199.129',
-  user: 'administrator',
-  pass: '12345678'
-});
-
-//  ssh.exec('sudo nmap -O localhost && uptime && free -m && ps -eo pid,comm,%mem,%cpu --sort=-%mem | head -n 6 && df -h /dev/sda2 && top -bn1 | grep "Cpu(s)" && ps aux | wc -l', {
-//    out: function(stdout) {
-//        console.log(stdout);
-//    }
-// }).start();
 
 // var j = schedule.scheduleJob('*/10 * * * * *', function(){
 // //   console.log('hello world');
-ssh
-  .exec('sudo nmap -O localhost && uptime && free -m && ps -eo pid,comm,%mem --sort=-%mem | head -n 6 && df -h /dev/sda2 && top -bn1 | grep "Cpu(s)" && ps aux | wc -l', {
-    pty: true,
-    exit: function(code, stdout, stderr){
-        if(stderr)
-        {
-            console.log(stderr);
-        }
+const { exec } = require('child_process');
+//var data = require('./getData');
+// let upDate = () => {
+    var script = exec('sudo ifstat -tSTz 10',
+    (error, stdout, stderr) => {
         console.log(stdout);
-        var p = new data(stdout);
-        var cpu = p.getCpu();
-        var os = p.getOS();
-        var port = p.getPort();
-        var load = p.getLoadAvg();
-        var mem = p.getMem();
-        var disk = p.getDisk();
-        var total = p.getTotalProcess();
-        var process = p.getProcess();
-  //       //console.log(os);
-  //       //console.log(port);
-  //       //console.log(load);
-  //       //console.log(mem);
-  //       //console.log(disk);
-  //       //console.log(total);
-  //       //console.log(process);
-            console.log(cpu);
-  }})
-.start();
-//});
-var  p = new data();
-p.getNetwork();
+    })
+//         var p = new data(stdout);
+//         var cpu = p.getCpu();
+//         //var os = p.getOS();
+//         var port = p.getPort();
+//         var load = p.getLoadAvg();
+//         var mem = p.getMem();
+//         var disk = p.getDisk();
+//         var total = p.getTotalProcess();
+//         var process = p.getProcess();
+//         var kill = p.killProcess();
+//         //console.log(port[0][4], port[0][2], port[0][1]);
+//         console.log(stderr);
+//         if (error !== null) {
+//             console.log(`exec error: ${error}`);
+//         }
+//     });
+
+//     //ip addr show | grep inet | grep 'host lo' => get ip address
+//     //lsb_release -a => get OS
+// }
+// setInterval(upDate,5000);
+//sudo lsof -i -P -n | grep LISTEN && uptime && free -m && ps -eo pid,comm,%mem,%cpu --sort=-%mem && df -h /dev/sda2 && top -bn1 | grep "Cpu(s)" && ps aux | wc -l
+
 
   //Lệnh cần dùng
   //Lấy ram [free -h]
