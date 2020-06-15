561aada341dca771524b649dbe901db101e167a9
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-06-16 02:11:53 +0700

    Update pushData.js

--- server/pushData.js
+++ server/pushData.js
@@ -1,19 +1,24 @@
 const firebase = require('firebase-admin');
 var serviceAccount = require("./service-account-file.json");
 const { exec } = require('child_process');
+var followCpu = require('./followCpu')
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://thaodoan-thesis.firebaseio.com"
 });
+
 var db = firebase.database();
 var store = firebase.firestore();
+//store databse path
 let sref = store.collection('001/domain/001');
+//firebase realtime database path
 var ref = db.ref("monitor/users");
 
 ref.once("value", function (snapshot) {
   console.log(snapshot.val());
 });
+//define path
 var uidRef = ref.child("001");
 var domainRef = uidRef.child("domain");
 var didRef = domainRef.child("001");
@@ -67,53 +72,56 @@ function pushData(data) {
     procRef.set(processList);
   }
   //Add Cpu Utilization
-  // pushData.prototype.addCpu = function () {
-  //   var { exec } = require('child_process');
-  //   exec('top -bn1 | grep "Cpu(s)"', (stdout, stderr) => {
-  //     console.log(stderr);
-  //     var reg = /(\d+|\d+.\d+)\s(id)/gim;
-  //     var temp = reg.exec(stdout);
-  //     var cpu = 100 - temp[1];
-  //     cpu = cpu.toFixed(2);
-  //     if (cpu > 70) {
-  //       followCpu();
-  //     }
-  //     sref.doc('cpu').set({
-  //       cpu: cpu
-  //     })
-  //   })
-  // }
+  pushData.prototype.addCpu = function () {
+    var { exec } = require('child_process');
+    exec('top -bn1 | grep "Cpu(s)"', (err, stdout, stderr) => {
+      console.log(stderr);
+      var reg = /(\d+|\d+.\d+)\s(id)/gim;
+      var temp = reg.exec(stdout);
+      var cpu = 100 - temp[1];
+      cpu = cpu.toFixed(2);
+      if (cpu > 70) {
+        followCpu();
+      }
+      console.log(cpu)
+      sref.doc('cpu').set({
+        cpus: cpu
+      })
+    })
+  }
   //Kill process
   let firstTime = true;
-    pushData.prototype.killProc = function () {
-      didRef.child("task/cmd").on('value', snapshot => {
-        if (firstTime) {
-          firstTime = false;
-          return;
-        }
-        // let cmd = snapshot.val();
-        // console.log(cmd);
-        var script = exec(snapshot.val());
+  pushData.prototype.killProc = function () {
+    didRef.child("task/cmd").on('value', snapshot => {
+      if (firstTime) {
+        firstTime = false;
+        return;
+      }
+      var script = exec(snapshot.val());
+      didRef.child("task/cmd").set({
+        cmd: ''
       })
-    }
-    //Add Networking
-    pushData.prototype.addNetwork = function () {
-      const { spawn } = require('child_process');
-      const ls = spawn('ifstat', ['-tSTz', '10']);
-      ls.stdout.on('data', data => {
-        console.log(`stdout: ${data}`);
-        var reg = /(\d+:\d+:\d+)\s+\d+.\d+\s+\d+.\d+\s+(\d+.\d+)\s+(\d+.\d+)$/gim;
-        var result = reg.exec(data);
-        sref.doc('network').set({
-          time: result[1],
-          in: result[2],
-          out: result[3]
-        });
-        console.log(result);
-      });
-      ls.stderr.on('data', data => {
-        console.log(`stderr: ${data}`);
-      });
-    };
+    })
   }
-  module.exports = pushData;
+  //Add Networking
+  pushData.prototype.addNetwork = function () {
+    const { spawn } = require('child_process');
+    const ls = spawn('ifstat', ['-tSTz', '10']);
+    ls.stdout.on('data', data => {
+      console.log(`stdout: ${data}`);
+      var reg = /(\d+:\d+:\d+)\s+\d+.\d+\s+\d+.\d+\s+(\d+.\d+)\s+(\d+.\d+)$/gim;
+      var result = reg.exec(data);
+      console.log(result[1]);
+      sref.doc('network').set({
+        time: result[1],
+        in: result[2],
+        out: result[3]
+      });
+      console.log(result);
+    });
+    ls.stderr.on('data', data => {
+      console.log(`stderr: ${data}`);
+    });
+  };
+}
+module.exports = pushData;
