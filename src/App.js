a2e2bcf53b1963306c3d00ceb54c57e8ffc37f4e
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-06-19 12:13:27 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './component/Home';
-import {app} from './firebase';
+import { app } from './firebase';
 import Login from './component/Login';
 class App extends Component {
   constructor() {
     super();
     this.state = ({
       user: null,
-      auth: null
+      email: ''
     });
     this.authListener = this.authListener.bind(this);
   }
@@ -18,26 +18,28 @@ class App extends Component {
   }
 
   authListener() {
-    let auth = app.auth()
-    auth.onAuthStateChanged((user) => {
+    app.auth().onAuthStateChanged((user) => {
       console.log(user);
+      let tmp = user.email
+      let reg = /(\w+)@\w+.\w+/gim;
+      var temp = reg.exec(tmp);
+      this.setState({
+        email: temp[1]
+      })
+      console.log(temp[1]);
       if (user) {
         this.setState({ user });
-        console.log(user);
         localStorage.setItem('user', user.uid);
       } else {
         this.setState({ user: null });
         localStorage.removeItem('user');
       }
     });
-    this.setState({
-      auth: auth 
-    })
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        {this.state.user ? ( <Home/>) : (<Login  auth={this.state.auth}/>)}
+        {this.state.user ? (<Home user = {this.state.email}/>) : (<Login />)}
       </div>
     );
   }
