cd97e34e1ac2ac11d18aeb3cc01d859639601c9d
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-06-21 21:33:50 +0700

    Add files via upload

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -1,12 +1,18 @@
-module.exports = function sendEmail (){
+module.exports = function sendEmail() {
     const nodemailer = require('nodemailer');
     require('dotenv').config()
 
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
-            user: process.env.Email,
-            pass: process.env.Email_Pass
+            type: 'OAuth2',
+            user: 'doanthao1503@gmail.com',
+            clientId: '565251832636-nblpn7hqppckrjrtfa3fmt406getkunb.apps.googleusercontent.com',
+            clientSecret: 'olE-g2j2nW0DPkROPZoTUtsI',
+            refreshToken: '1//04xJjZ7HGyiOUCgYIARAAGAQSNwF-L9IrrI806Dz0Fff-9_g1H30fR4goxHaYBjG5n0AHjsa4bimxVtfWJVhtY0Pehd0sbxeX7_4',
+            accessToken: 'ya29.a0AfH6SMBbI592SzNaD1K1VnyA5XsgHKArAiLE7U_k4Z7UHIWLlnf9Psf2rN0NOq1ILY_arXeh13HgTO2TIyfH0MmXKpRomMM2dNMDRgYbYnxGHHF641803YeqHSFvHnsUqlrSHbEzC0MiH9rR9-um3UmXgvswZV84fd4'
         }
     });
 
@@ -18,10 +24,11 @@ module.exports = function sendEmail (){
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
-        // if (error) {
-        //   console.log(error);
-        // } else {
-        //   console.log('Email sent: ' + info.response);
-        // }
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            console.log('Preview URL:', nodemailer.getTestMessageUrl(mailOptions));
+        }
     });
 }
